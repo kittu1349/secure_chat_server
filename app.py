639e4a88dcc4d44e0013106c16dad33b1d452f65
kittu <72201419+kittu1349@users.noreply.github.com>
2024-06-28 20:20:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,18 @@ from flask import Flask, redirect, url_for, render_template, request, flash, ses
 # we use flask session to handle all login related functions instead of flask-login
 # we use this to save information in a database
 from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
 # hashing and salting the password
 from flask_bcrypt import Bcrypt
 import random
-from string import digits,ascii_uppercase 
+from string import digits, ascii_uppercase
+from flask_admin import Admin, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
 
 # to create secret key for session
 def generate_secertkey():
     n = 16
-    key  = "".join(random.choices(ascii_uppercase+digits,k=n))
+    key = "".join(random.choices(ascii_uppercase + digits, k=n))
     return str(key)
 
 
@@ -20,6 +23,9 @@ app.secret_key = generate_secertkey()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 # below, so we dont track all modifications
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# to expire the session after some time
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=20)
+
 bcrypt = Bcrypt(app)
 
 # to create a database
@@ -30,6 +36,25 @@ class Users(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     username = db.Column("username", db.String(100), unique=True, nullable=False)
     password = db.Column("password", db.String(100), nullable=False)
+    admin = db.Column("admin", db.Boolean, default=False)
+
+# access only to logged in users and if they are admin
+class MyAdminIndexView(AdminIndexView):
+    def is_accessible(self):
+        return "user" in session and session.get("admin")
+
+    def inaccessible_callback(self, name, **kwargs):
+        flash("You are not authorized to access this page!")
+        return redirect(url_for("login"))
+
+# below hides the password column on admin panel
+class UserAdmin(ModelView):
+    column_exclude_list = ('password',)
+    form_excluded_columns = ('password',)
+
+# creating admin with ability to be accessed only when logged in and hides password column
+admin = Admin(app, index_view=MyAdminIndexView())
+admin.add_view(UserAdmin(Users, db.session))
 
 @app.route("/home")
 @app.route("/")
@@ -38,18 +63,19 @@ def home():
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
-    if request.method == "POST": 
+    if request.method == "POST":
         # function that gets the nm value from the post request and creates a session cookie using that username
         username = request.form.get('username')
         password = request.form.get('password')
-        found_user = Users.query.filter_by(username=username).first()   
+        found_user = Users.query.filter_by(username=username).first()
         if found_user and bcrypt.check_password_hash(found_user.password, password):
             session["user"] = username
-            flash("Login Successful!!")
+            session["admin"] = found_user.admin
             # redirects to user dashboard
+            flash("Login Successful!!")
             return redirect(url_for("dashboard"))
         else:
-            # if username/password dont exist or are wrong
+            # if username/password don't exist or are wrong
             flash("Invalid username or password.")
     if "user" in session:  # when /login, checks for session cookies, if present leads to dashboard, else to login page
         flash("Already Logged in!!")
@@ -89,6 +115,7 @@ def dashboard():
 def logout():
     if "user" in session:  # assigns the session cookie to the user dashboard
         session.pop("user", None)
+        session.pop("admin", None)
         flash("Logged out!!", "info")  # only flashes the message once after logout, not more than that
     else:
         # if already logged out
